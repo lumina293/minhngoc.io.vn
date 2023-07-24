0673b9427c780b79dbccc49198008ac29405d330
lumina293 <luongminhngoc29@gmail.com>
2023-07-24 12:49:00 +0800

    Fix build failed because of escaped characters

--- app/page.js
+++ app/page.js
@@ -3,8 +3,8 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-3xl">Hi there, I'm Minh Ngoc, also called Mina</h1>
-      <p>I'm learning to code</p>
+      <h1 className="text-3xl">Hi there, I&apos;m Minh Ngoc, also called Mina</h1>
+      <p>I&apos;m learning to code</p>
     </main>
   )
 }
